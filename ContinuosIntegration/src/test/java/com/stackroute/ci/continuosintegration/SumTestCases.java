691c83fef5680795ad3d35c6410a6f3aab0e150c
Niyaz <nyz.git@gmail.com>
2017-07-29 10:55:47 +0530

    added files

---
+++ ContinuosIntegration/src/test/java/com/stackroute/ci/continuosintegration/SumTestCases.java
@@ -0,0 +1,23 @@
+package com.stackroute.ci.continuosintegration;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SumTestCases {
+
+	private static App app;
+	
+	@BeforeClass
+	public static void init() throws Exception {
+		
+		app = new App();
+	}
+	
+	@Test
+	public void AddtionTestCase() {
+		assertEquals(20,app.sum(10, 10));
+	}
+
+}
